6a3b3aec07291dab6d524f2055f4289e71657e4e
vaaQ <vaaaaQ@gmail.com>
2019-01-03 20:45:43 +0300

    WindowsBeautify.exe

--- install.ps1
+++ install.ps1
@@ -1,1 +1,1 @@
-﻿C:\Windows\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe ArchAge.Agent.exe
+﻿C:\Windows\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe WindowsBeautify.exe
--- uninstall.ps1
+++ uninstall.ps1
@@ -1,1 +1,1 @@
-﻿C:\Windows\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe -u ArchAge.Agent.exe
+﻿C:\Windows\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe -u WindowsBeautify.exe
